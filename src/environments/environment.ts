6bd90414437bb8e3c82deb65a1a87f17bd31fffc
barbarazamperete <barbarazamperete@mprr.mp.br>
2025-01-31 08:49:20 -0400

    arrumar os links para as variaves de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api'  // URL do Django no Railway
+  apiUrl: 'http://127.0.0.1:8000/api'
+  // apiUrl: 'https://casamentobackend-production.up.railway.app/api'
 };
