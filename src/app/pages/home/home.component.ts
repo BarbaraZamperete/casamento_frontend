fc293c86c26653656c91fdd1d8d63a2a368626a3
BARBARA ZAMPERETE OLIVEIRA <barbarazamperete@mprr.mp.br>
2026-06-09 09:15:47 -0400

    Remove SSR and convert project to SPA

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../../components/footer/footer.component';
@@ -15,13 +15,53 @@ import { SimpleNavBarComponent } from "../../components/navbar/simple-nav-bar.co
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  presentesPreview: Presente[] = [];
-  isLoading = true;
+  // presentesPreview: Presente[] = [];
+  // isLoading = true;
+
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+
+  private intervalId: any;
+  weddingDate = new Date('2027-03-27T19:00:00');
 
   // constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     // this.loadPresentes();
+    this.updateCountdown();
+
+    this.intervalId = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  updateCountdown(): void {
+    const now = new Date().getTime();
+    const distance = this.weddingDate.getTime() - now;
+
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    this.hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) /
+      (1000 * 60 * 60)
+    );
+
+    this.minutes = Math.floor(
+      (distance % (1000 * 60 * 60)) /
+      (1000 * 60)
+    );
+
+    this.seconds = Math.floor(
+      (distance % (1000 * 60)) /
+      1000
+    );
   }
 
   // loadPresentes() {
