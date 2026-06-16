35bafc1305ad8cf966df8f59a6576d9b73aabcfb
BARBARA ZAMPERETE OLIVEIRA <barbarazamperete@mprr.mp.br>
2026-06-16 14:56:50 -0400

    mudança das titulos

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -20,14 +20,14 @@ export class TimelineComponent {
   timelineItems: TimelineItem[] = [
     {
       date: '16/01/2021',
-      title: 'Primeira troca de mensagens',
+      title: 'Uma troca de mensagens',
       description: `
       <p>Bárbara fez uma tatuagem e postou nos stories. Kamila, que também tinha interesse em fazer uma tatuagem um dia, aproveitou a oportunidade para puxar assunto e conversar sobre a experiência, perguntando como era a dor. E assim começou a conversa entre elas. Alguns anos depois, Bárbara daria de presente a Kamila a realização de sua primeira tatuagem.</p>`,
       image: 'assets/images/default-image.png'
     },
     {
       date: '27/03/2021',
-      title: 'Primeiro beijo',
+      title: 'Um beijo e tantos mais',
       description: `
       <p>Após alguns meses trocando apenas mensagens e videochamadas, já que a pandemia de COVID-19 ainda estava em andamento, Bárbara e Kamila marcaram seu primeiro encontro na gelateria Dois e 90. Após o sorvete, foram para a Praça do Mirandinha, de onde acabaram sendo “expulsas” por um fiscal devido às restrições da pandemia. Então resolveram ir para a Praça do Chefão. </p>
       <p>Sentadas na arquibancada de frente para a quadra de tênis, Kamila começou a fazer as perguntinhas que havia planejado para quebrar o gelo e conduzir o encontro ao objetivo final. A última pergunta era: “Em que lugar você gostaria de ser beijada?”.</p>
@@ -36,7 +36,7 @@ export class TimelineComponent {
     },
     {
       date: '18/06/2021',
-      title: 'Pedido de namoro',
+      title: 'O início de nós',
       description: `
       <p>Após o primeiro beijo, as duas nunca mais deixaram de se ver. Toda semana havia encontrinhos, todos cheios de surpresinhas românticas, desenhos, chocolates, poemas, músicas e brincadeiras. Em junho, Bárbara viajou, e as duas passaram algumas semanas conversando por longas videochamadas. Nesse tempo, o coração finalmente se abriu por completo, e a paixão floresceu entre elas.</p>
       <p>Ao retornar da viagem, Kamila a presenteou com uma caneca de gatinhos e vários coraçõeszinhos de papel, cada um com uma palavra formando a frase: “Quem sabe você quer namorar comigo?”. É claro que Bárbara disse que sim.</p>`,
@@ -44,7 +44,7 @@ export class TimelineComponent {
     },
     {
       date: '25/12/2021',
-      title: 'Eu te amo',
+      title: 'No silêncio, o amor ecoou',
       description: `
       <p>Em uma tarde de um dia qualquer, após alguns meses de namoro, Kamila encontrava-se deitada nos braços de Bárbara. Estavam conversando tranquilamente quando Bárbara sentiu vontade de revelar um sentimento, porém, é claro, de uma forma um tanto quanto diferente. Ela desenhou nas costas de Kamila as letras da frase “Eu te amo”, enquanto Kamila ia soletrando até compreender a mensagem por completo.</p>
       <p>No Natal daquele ano, Kamila estava em viagem para Manaus. Mesmo à distância, gravou um vídeo segurando placas e, à medida que as mostrava, revelava que também amava Bárbara.</p>`,
@@ -52,7 +52,7 @@ export class TimelineComponent {
     },
     {
       date: '2021 e contando',
-      title: 'Aventuras',
+      title: 'Construindo memórias',
       description: `
       <p>Durante os anos de namoro, Kamila e Bárbara viveram muitas aventuras, viagens e perrengues que depois viraram histórias para contar (como a caminhada no Morro da Tijuca e o episódio em que ficaram perdidas à noite no meio da estrada no Rio Grande do Norte, entre outros). Também enfrentaram desafios que se transformaram em aprendizados e contribuíram para a evolução do casal.</p>
       <p>Além disso, viveram muitos encontros especiais, compartilharam diversos jogos e brincadeiras (como as Olimpíadas das Namoradas, Overcooked e jogos de tabuleiro, entre outros) e colecionaram momentos inesquecíveis. Foram todas essas experiências que ajudaram a construir a relação de duas mulheres muito companheiras, melhores amigas e com uma conexão única.</p>`,
@@ -60,7 +60,7 @@ export class TimelineComponent {
     },
     {
       date: '30/09/2024',
-      title: 'Morar juntas',
+      title: 'Nosso lugarzinho',
       description: `
       <p>Em setembro de 2024, Kamila e Bárbara resolveram dar um próximo passo em sua relação e juntaram oficialmente as escovas de dentes. Essa mudança de fase veio acompanhada de muitos desafios, mas elas se mantiveram unidas e firmes, pois são mais fortes assim.</p>
       <p>Também foi uma fase que trouxe muito aprendizado, evolução e inúmeras experiências positivas para o casal. Entre elas, a decisão de aumentar a família com a chegada de duas gatinhas muito especiais: Collina e Ada.</p>
@@ -69,7 +69,7 @@ export class TimelineComponent {
     },
     {
       date: '01/01/2025',
-      title: 'Noivado',
+      title: 'O sim',
       description: `
       <p>No réveillon, sob o brilho dos fogos de artifício e perante as pessoas mais importantes da família das duas, Kamila se declarou para Bárbara, ajoelhou-se no gramado e a pediu em casamento. Não havia nenhuma dúvida: Bárbara disse sim!</p>
       <p>No entanto, Bárbara acreditava que Kamila também deveria viver a experiência de ser pedida em casamento. Alguns meses depois, durante uma viagem para o Rio Grande do Norte, na cidade de São Miguel do Gostoso, Bárbara organizou para que o quarto estivesse todo decorado e fez sua declaração quando chegaram à hospedagem.</p>
@@ -78,7 +78,7 @@ export class TimelineComponent {
     },
     {
       date: '27/03/2027',
-      title: 'Casamento',
+      title: 'Para sempre nós',
       description: `
       <p>E enfim chegamos a mais um grande marco na história desse casal, que tem a felicidade de poder comemorar este momento ao lado de pessoas tão especiais.</p>
       <p>Tudo começou com uma simples conversa sobre uma tatuagem. Depois vieram os encontros, os bilhetinhos, os chocolates, as videochamadas, as viagens, as aventuras, os aprendizados, as conquistas, os desafios, os gatos, a casa compartilhada e o sonho de construir uma vida juntas.</p>
